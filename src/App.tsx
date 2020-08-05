f2401fb26b1c97a0e00856f8e4706838c45069b8
Egor <rank2007@mail.ru>
2020-08-05 22:02:19 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
+import s from "./App.module.css"
 import {NavLink} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  )
+    return (
+        <div className={s.container}>
+            <NavLink to={"/prejunior"}>preJunior</NavLink>
+            <NavLink to={"/junior"}>Junior</NavLink>
+            <NavLink to={"/junior+"}>Junior+</NavLink>
+        </div>
+    )
 }
 
 export default App;
